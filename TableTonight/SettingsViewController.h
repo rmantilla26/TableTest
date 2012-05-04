2f5981b28d12f08a76aa0a661451c3ef4d8b1568
bryan <bolivarbryan@gmail.com>
2012-05-04 09:51:58 -0500

    settings

---
+++ TableTonight/SettingsViewController.h
@@ -0,0 +1,16 @@
+//
+//  SettingsViewController.h
+//  TableTonight
+//
+//  Created by Koombea Tester on 5/4/12.
+//  Copyright (c) 2012 Koombea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+
+- (IBAction)gotoMainMenu;
+
+@end
---
+++ TableTonight/SettingsViewController.m
@@ -0,0 +1,49 @@
+//
+//  SettingsViewController.m
+//  TableTonight
+//
+//  Created by Koombea Tester on 5/4/12.
+//  Copyright (c) 2012 Koombea. All rights reserved.
+//
+
+#import "SettingsViewController.h"
+
+@interface SettingsViewController ()
+
+@end
+
+@implementation SettingsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+
+- (IBAction)gotoMainMenu{
+    
+}
+
+
+@end
