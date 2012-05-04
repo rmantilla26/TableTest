a0135b9accbe32f1c3932162be7c44de0639fcbb
Raul Mantillla <rmantilla26@hotmail.com>
2012-05-04 10:02:23 -0500

    second test

---
+++ TableTonight/GameViewController.m
@@ -0,0 +1,48 @@
+//
+//  GameViewController.m
+//  TableTonight
+//
+//  Created by Raul Mantilla on 5/4/12.
+//  Copyright (c) 2012 Koombea. All rights reserved.
+//
+
+#import "GameViewController.h"
+
+@interface GameViewController ()
+
+@end
+
+@implementation GameViewController
+
+
+-(void)closeWind{
+    [self dismissModalViewControllerAnimated:YES];
+}
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
